3fd58547058d4c7bb3745523b967ada55b347bd3
anjanipodomi <anjanidihapsari@gmail.com>
2024-04-23 10:49:08 +0700

    menambahkan prosedur void input

--- objek/objek.cpp
+++ objek/objek.cpp
@@ -20,5 +20,12 @@ private:
 	string namaMK;
 	int sks;
 public:
-
+	void input() {
+		cout << "Kode MK: ";
+		cin >> kodeMK;
+		cout << "Nama MK: ";
+		cin >> namaMK;
+		cout << "SKS: ";
+		cin >> sks;
+	}
 };
